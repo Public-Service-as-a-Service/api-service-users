97d6a5f0f2989b068b4b0da3af64a2a68eb64706
Simon-n-o <simon.n.olsson@gmail.com>
2025-05-06 10:04:45 +0200

    started implementing flyway + documentation updates

---
+++ src/main/resources/db/migrations/V1__add_database_schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users (
+    guid VARCHAR(100) NOT NULL DEFAULT (UUID()),
+    email_address VARCHAR(50) NOT NULL PRIMARY KEY,
+    phone_number_ VARCHAR(15),
+	municipality_id VARCHAR(50),
+    status ENUM('ACTIVE', 'SUSPENDED', 'INACTIVE')
+);
