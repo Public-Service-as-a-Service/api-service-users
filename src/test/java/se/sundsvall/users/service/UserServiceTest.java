00b24e5b9152d9c69fa9bc8d77965226305ef89b
mayx17 <martiny@bahnhof.se>
2025-05-07 15:10:19 +0200

    Update UserServiceTest.java

--- src/test/java/se/sundsvall/users/service/UserServiceTest.java
+++ src/test/java/se/sundsvall/users/service/UserServiceTest.java
@@ -1,26 +1,22 @@
 package se.sundsvall.users.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockedStatic;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.zalando.problem.Problem;
 import se.sundsvall.users.api.model.UpdateUserRequest;
 import se.sundsvall.users.api.model.UserRequest;
 import se.sundsvall.users.api.model.UserResponse;
 import se.sundsvall.users.integration.UserRepository;
+import se.sundsvall.users.integration.model.Enum.Status;
 import se.sundsvall.users.integration.model.UserEntity;
 import se.sundsvall.users.service.Mapper.UserMapper;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.eclipse.sisu.inject.Logs.catchThrowable;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.same;
@@ -63,25 +59,25 @@ public class UserServiceTest {
 	@Test
 	void createUser() {
 		// Arrange
-		final var id = "TestMail123@mail.se";
+		final var email = "TestMail123@mail.se";
 		final var phoneNumber = "99070121212";
 		final var municipalityId = "2281";
 		final var status = "Active";
-		final var userRequestMock = UserRequest.create().withEmail(id)
+		final var userRequestMock = UserRequest.create().withEmail(email)
 			.withPhoneNumber(phoneNumber)
 			.withMunicipalityId(municipalityId)
 			.withStatus(status);
-		final var userEntity = UserEntity.create().withEmail(id)
+		final var userEntity = UserEntity.create().withEmail(email)
 			.withPhoneNumber(phoneNumber)
 			.withMunicipalityId(municipalityId)
 			.withStatus(status);
-		final var userResponseMock = UserResponse.create().withEmail(id)
+		final var userResponseMock = UserResponse.create().withEmail(email)
 			.withPhoneNumber(phoneNumber)
 			.withMunicipalityId(municipalityId)
 			.withStatus(status);
 
 		when(userRepositoryMock.save(userEntity)).thenReturn(userEntity);
-		when(userRepositoryMock.findById(id)).thenReturn(Optional.empty());
+		when(userRepositoryMock.findById(email)).thenReturn(Optional.empty());
 
 		when(userMapper.toUserEntity(userRequestMock)).thenReturn(userEntity);
 		when(userMapper.toUserResponse(userEntity)).thenReturn(userResponseMock);
@@ -97,12 +93,66 @@ public class UserServiceTest {
 	}
 
 	@Test
+	void updateUser() {
+		// Arrange
+		final var email = "TestMail123@mail.se";
+		final var phoneNumber = "0701235223";
+		final var municipalityId = "2281";
+		final var status = "Active";
+		final var userRequestMock = UpdateUserRequest.create()
+			.withPhoneNumber(phoneNumber)
+			.withMunicipalityId(municipalityId)
+			.withStatus(status);
+		final var userEntity = UserEntity.create().withEmail(email)
+			.withPhoneNumber(phoneNumber)
+			.withMunicipalityId(municipalityId)
+			.withStatus(status);
+		final var userResponseMock = UserResponse.create().withEmail(email)
+			.withPhoneNumber(phoneNumber)
+			.withMunicipalityId(municipalityId)
+			.withStatus(status);
+		// Mock
+		when(userRepositoryMock.findById(email)).thenReturn(Optional.of(userEntity));
+		when(userRepositoryMock.getReferenceById(email)).thenReturn(userEntity);
+
+		when(userRepositoryMock.save(userEntity)).thenReturn(userEntity);
+		when(userMapper.toUserResponse(userEntity)).thenReturn(userResponseMock);
+
+		// Act
+		final var updatedUser = userService.updateUser(userRequestMock, email);
+
+		// Verify/Assert
+		verify(userRepositoryMock).save(same(userEntity));
+		assertThat(updatedUser).isNotNull();
+		assertThat(updatedUser).isEqualTo(userResponseMock);
+	}
+
+	@Test
+	void deleteUser() {
+
+		// Arrange
+		final var email = "TestMail123@mail.se";
+		final var userEntity = UserEntity.create().withEmail(email);
+
+		// Mock
+		when(userRepositoryMock.findById(email)).thenReturn(Optional.of(userEntity));
+		when(userRepositoryMock.getReferenceById(email)).thenReturn(userEntity);
+
+		// Act
+		userService.deleteUser(email);
+
+		// Verify/Assert
+		verify(userRepositoryMock).deleteById(email);
+	}
+
+	@Test
 	void createUserAlreadyExists() {
 		// Arrange
 		final var email = "TestMail123@mail.se";
 		final var userRequest = new UserRequest();
 		userRequest.setEmail(email);
 
+		// Mock
 		when(userRepositoryMock.findById(email)).thenReturn(Optional.of(new UserEntity()));
 
 		// Act & Assert
