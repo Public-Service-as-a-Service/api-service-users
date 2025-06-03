cd8430f87046c814b5661d8ec4ec8ce688f35f8e
Simon-n-o <simon.n.olsson@gmail.com>
2025-06-03 14:47:43 +0200

    Added integraton tests for getting users

---
+++ src/integration-test/java/se/sundsvall/users/apptest/DeleteUserIT.java
@@ -0,0 +1,4 @@
+package se.sundsvall.users.apptest;
+
+public class DeleteUserIT {
+}
---
+++ src/integration-test/java/se/sundsvall/users/apptest/GetUserIT.java
@@ -0,0 +1,79 @@
+package se.sundsvall.users.apptest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import se.sundsvall.dept44.test.AbstractAppTest;
+import se.sundsvall.dept44.test.annotation.wiremock.WireMockAppTestSuite;
+import se.sundsvall.users.Application;
+import se.sundsvall.users.integration.db.UserRepository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@WireMockAppTestSuite(files = "classpath:/GetUserIT/", classes = Application.class)
+@Sql(scripts = {
+        "/db/script/truncate.sql",
+        "/db/script/GetUserTest.sql"
+})
+@ActiveProfiles("default")
+
+class GetUserIT extends AbstractAppTest {
+    private static final String RESPONSE = "response.json";
+    @Autowired
+    UserRepository userRepository;
+
+    @Test
+    void test07_getUserByEmail() {
+
+        final String email = "testmail1@sundsvall.se";
+
+        assertThat(userRepository.findByEmail(email)).isPresent();
+
+        setupCall()
+                .withServicePath("/api/users/email/".concat(email))
+                .withHttpMethod(HttpMethod.GET)
+                .withExpectedResponseStatus(HttpStatus.OK)
+                .withExpectedResponse(RESPONSE)
+                .sendRequestAndVerifyResponse();
+
+
+    }
+
+    @Test
+    void test08_getUserByPartyId() {
+
+        final String partyId = "7225dc69-28d1-4064-a1a8-5c1de5da0e62";
+
+        assertThat(userRepository.findByPartyId(partyId)).isPresent();
+
+        setupCall()
+                .withServicePath("/api/users/partyIds/".concat(partyId))
+                .withHttpMethod(HttpMethod.GET)
+                .withExpectedResponseStatus(HttpStatus.OK)
+                .withExpectedResponse(RESPONSE)
+                .sendRequestAndVerifyResponse();
+
+
+    }
+
+    @Test
+    void test09_getUserByPersonNumber() {
+
+        final String personNumber = "198001011234";
+        final String partyId = "";
+        final String municipalityId = "2281";
+
+
+        assertThat(userRepository.findById(personNumber));
+
+        setupCall()
+                .withServicePath("/api/users/personalNumbers/198001011234?municipalityId=2281")                .withHttpMethod(HttpMethod.GET)
+                .withExpectedResponseStatus(HttpStatus.OK)
+                .withExpectedResponse(RESPONSE)
+                .sendRequestAndVerifyResponse();
+
+    }
+}
